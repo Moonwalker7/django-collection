d9915346be8cf4e027287db2876709d60071107a
Savio <joelbajpai7@gmail.com>
2023-02-12 19:12:36 +0530

    Add a Template for Detail User Http404 to handle if id does not exist

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -13,8 +13,12 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    response = f"You're looking at the question {question_id}"
-    return HttpResponse(response)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question Does Not Exist')
+
+    return render(request, 'polls/detail.html',{'question': question})
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}"
