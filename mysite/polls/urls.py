3442fddc6b7f4f1b92a996d69be7e2720efc9098
Savio <joel.bajpai7@gmail.com>
2023-02-13 20:11:40 +0530

    Comment Vote URL for now

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/detail/', detail, name='detail'),
     path('<int:question_id>/results/', results, name='results'),
-    path('<int:question_id>/vote/', vote, name='vote')
+    # path('<int:question_id>/vote/', vote, name='vote')
 ]
 
